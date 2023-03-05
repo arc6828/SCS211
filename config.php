534bf13906a2d78d99968a9dc52e9800908f4e51
Chavalit Koweerawong <chavalit.kow@gmail.com>
2023-03-05 11:56:07 +0700

    Update config.php

--- config.php
+++ config.php
@@ -13,3 +13,8 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+// echo "Initial character set is: " . mysqli_character_set_name($link);
+
+// Change character set to utf8 (support Thai character)
+mysqli_set_charset($link,"utf8");
