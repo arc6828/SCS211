24bf5c59127a1ed2d85ac501a5b19824913b0f9a
Chavalit Koweerawong <chavalit.kow@gmail.com>
2023-05-28 14:23:49 +0700

    Update employees.sql

--- employee/employees.sql
+++ employee/employees.sql
@@ -9,15 +9,3 @@ INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
 (1, 'ชวลิต', 'มหาวิทยาลัย', 100),
 (2, 'Ronald', 'USA', 200);
 
---
--- Indexes for table `employees`
---
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for table `employees`
---
-ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
