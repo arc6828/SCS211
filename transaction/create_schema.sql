26a146e06aaa84d5e4808977bcc768691d379bb1
arc6828 <chavalit.kow@gmail.com>
2023-08-21 13:29:02 +0700

    Create create_schema.sql

---
+++ transaction/create_schema.sql
@@ -0,0 +1,34 @@
+-- Create the 'accounts' table
+CREATE TABLE accounts (
+    account_number VARCHAR(10) PRIMARY KEY,
+    account_holder VARCHAR(50),
+    balance DECIMAL(10, 2)
+);
+
+-- Insert sample data into the 'accounts' table
+INSERT INTO
+    accounts (account_number, account_holder, balance)
+VALUES
+    ('123456', 'John Doe', 2500.00),
+    ('987654', 'Jane Smith', 1500.00);
+
+-- Create the 'transactions' table
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY,
+    account_number VARCHAR(10),
+    amount DECIMAL(10, 2),
+    transaction_type VARCHAR(20),
+    FOREIGN KEY (account_number) REFERENCES accounts(account_number)
+);
+
+-- Insert sample data into the 'transactions' table
+INSERT INTO
+    transactions (
+        transaction_id,
+        account_number,
+        amount,
+        transaction_type
+    )
+VALUES
+    (1, '123456', 500.00, 'Withdrawal'),
+    (2, '987654', 500.00, 'Deposit');
